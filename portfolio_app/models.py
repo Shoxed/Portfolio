ae66efaf1634103cb10f8de64cadb3d434c91c34
Jonathan Hickey <jhickey2021@gmail.com>
2023-09-30 15:04:43 -0600

    Names in Admin Portal Now Visible

--- portfolio_app/models.py
+++ portfolio_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 class Student(models.Model):
@@ -16,3 +17,11 @@ class Student(models.Model):
     name = models.CharField(max_length=200)
     email = models.CharField("UCCS Email", max_length=200)
     major = models.CharField(max_length=200, choices=MAJOR)
+    #Define default String to return the name for representing the Model object."
+    def __str__(self):
+        return self.name
+    #Returns the URL to access a particular instance of MyModelName.
+    #if you define this method then Django will automatically
+    #add a "View on Site" button to the model's record editing screens in the Admin site
+    def get_absolute_url(self):
+        return reverse('student-detail', args=[str(self.id)])
