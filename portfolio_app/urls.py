6a9e5def9b85e54b24407ed2c4298ec925c9091e
Jonathan Hickey <jhickey2021@gmail.com>
2023-10-16 12:16:57 -0600

    Added HTML and Views for Student List

--- portfolio_app/urls.py
+++ portfolio_app/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name = 'index'),
+    path('students/', views.StudentListView.as_view(), name= 'students'),
+    path('student/<int:pk>', views.StudentDetailView.as_view(), name='student-detail'),
 ]
--- portfolio_app/views.py
+++ portfolio_app/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from .models import *
+from django.views import generic
+#from .forms import ProjectForm, PortfolioForm
+from django.contrib import messages
 # Create your views here.
+class StudentListView(generic.ListView):
+    model = Student
+
+class StudentDetailView(generic.DetailView):
+    model = Student
 
 def index(request):
     return render (request, 'portfolio_app/index.html')
