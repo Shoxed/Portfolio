37dcdac935cb5158eb3f8031a97c6fdc61d9f691
Jonathan Hickey <jhickey2021@gmail.com>
2023-10-17 00:26:38 -0600

    Added HTML for Portfolio

--- portfolio_app/views.py
+++ portfolio_app/views.py
@@ -23,4 +23,6 @@ class PortfolioDetailView(generic.DetailView):
 
 
 def index(request):
-    return render (request, 'portfolio_app/index.html')
+    student_active_portfolios = Student.objects.select_related('portfolio').all().filter(portfolio__is_active=True)
+    print("active portfolio query set", student_active_portfolios)
+    return render( request, 'portfolio_app/index.html', {'student_active_portfolios':student_active_portfolios})
