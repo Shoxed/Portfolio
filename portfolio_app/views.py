04a3a793d769db33bd283d1aaf1bfae2f08f29b3
Jonathan Hickey <jhickey2021@gmail.com>
2023-10-17 00:15:03 -0600

    Added Views and URL Paths for Portfolio

--- portfolio_app/views.py
+++ portfolio_app/views.py
@@ -6,12 +6,21 @@ from .models import *
 from django.views import generic
 #from .forms import ProjectForm, PortfolioForm
 from django.contrib import messages
+
+
 # Create your views here.
 class StudentListView(generic.ListView):
     model = Student
 
 class StudentDetailView(generic.DetailView):
     model = Student
+
+class PortfolioListView(generic.ListView):
+    model = Portfolio
+
+class PortfolioDetailView(generic.DetailView):
+    model = Portfolio
+
 
 def index(request):
     return render (request, 'portfolio_app/index.html')
