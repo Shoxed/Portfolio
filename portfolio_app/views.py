c083c83410a2735958e1c14803260aa9fa8e1d3d
Jonathan Hickey <jhickey2021@gmail.com>
2023-10-17 20:50:46 -0600

    Added elements for Forms

--- portfolio_app/views.py
+++ portfolio_app/views.py
@@ -6,6 +6,7 @@ from .models import *
 from django.views import generic
 #from .forms import ProjectForm, PortfolioForm
 from django.contrib import messages
+from .forms import ProjectForm
 
 
 # Create your views here.
@@ -38,3 +39,26 @@ def index(request):
     student_active_portfolios = Student.objects.select_related('portfolio').all().filter(portfolio__is_active=True)
     print("active portfolio query set", student_active_portfolios)
     return render( request, 'portfolio_app/index.html', {'student_active_portfolios':student_active_portfolios})
+
+def createProject(request, portfolio_id):
+    form = ProjectForm()
+    portfolio = Portfolio.objects.get(pk=portfolio_id)
+    
+    if request.method == 'POST':
+        # Create a new dictionary with form data and portfolio_id
+        project_data = request.POST.copy()
+        project_data['portfolio_id'] = portfolio_id
+        
+        form = ProjectForm(project_data)
+        if form.is_valid():
+            # Save the form without committing to the database
+            project = form.save(commit=False)
+            # Set the portfolio relationship
+            project.portfolio = portfolio
+            project.save()
+
+            # Redirect back to the portfolio detail page
+            return redirect('portfolio-detail', portfolio_id)
+
+    context = {'form': form}
+    return render(request, 'portfolio_app/project_form.html', context)
